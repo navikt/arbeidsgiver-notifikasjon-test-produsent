f9c569a9617053cbc7ca12e9c63b5ced4e4b9072
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-08-26 14:23:56 +0200

    try to get aad token

--- src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/test_produsent/main.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/test_produsent/main.kt
@@ -1,5 +1,10 @@
 package no.nav.arbeidsgiver.notifikasjon.test_produsent
 import io.ktor.application.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
@@ -11,6 +16,13 @@ fun main() {
     val log = LoggerFactory.getLogger("main")
     log.info("hello world")
 
+    val client = HttpClient(Apache)
+    val tokenEndpoint = System.getenv("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT")!!
+    val tenantId = System.getenv("AZURE_APP_TENANT_ID")!!
+    val clientId = System.getenv("AZURE_APP_CLIENT_ID")!!
+    val clientSecret = System.getenv("AZURE_APP_CLIENT_SECRET")!!
+
+
     embeddedServer(Netty, port = 8080) {
         routing {
             get("ok") {
@@ -19,7 +31,21 @@ fun main() {
 
             get("doStuff") {
                 log.info("doing stuff")
-                call.respondText("stuff done")
+
+                val accessToken = client.submitForm<String>(
+                    url = tokenEndpoint,
+                    formParameters = Parameters.build {
+                        append("tenant", tenantId)
+                        append("client_id", clientId)
+                        append("scope", "api://dev-gcp.fager.notifikasjon-produsent-api/.default\n")
+                        append("client_secret", clientSecret)
+                        append("grant_type", "client_credentials")
+                    }
+                ) {
+                    method = HttpMethod.Post
+                }
+
+                call.respondText(accessToken)
             }
         }
     }.start(wait = true)
