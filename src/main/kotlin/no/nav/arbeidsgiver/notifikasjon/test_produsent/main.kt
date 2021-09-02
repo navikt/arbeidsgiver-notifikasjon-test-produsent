cafb84e510c4ff15653c961dbcfe59bed86d13df
Ken Gullaksen <ken.gullaksen@nav.no>
2021-09-02 10:42:40 +0200

    produser test notifikasjon

--- src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/test_produsent/main.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/notifikasjon/test_produsent/main.kt
@@ -1,16 +1,24 @@
 package no.nav.arbeidsgiver.notifikasjon.test_produsent
+
 import io.ktor.application.*
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import org.slf4j.LoggerFactory
+import java.util.*
 
+fun <T>basedOnEnv(prod: T, other: T): T =
+    when (System.getenv("NAIS_CLUSTER_NAME")) {
+        "prod-gcp" -> prod
+        else -> other
+    }
 
 fun main() {
     val log = LoggerFactory.getLogger("main")
@@ -38,14 +46,54 @@ fun main() {
                         formParameters = Parameters.build {
                             append("tenant", tenantId)
                             append("client_id", clientId)
-                            append("scope", "api://dev-gcp.fager.notifikasjon-produsent-api/.default\n")
+                            append("scope", basedOnEnv(
+                                prod = "api://prod-gcp.fager.notifikasjon-produsent-api/.default",
+                                other = "api://dev-gcp.fager.notifikasjon-produsent-api/.default"
+                            ))
                             append("client_secret", clientSecret)
                             append("grant_type", "client_credentials")
                         }
                     ) {
                         method = HttpMethod.Post
                     }
-                    call.respondText(accessToken)
+
+                    val response: HttpResponse = client.post("http://notifikasjon-produsent-api/api/graphql") {
+                        headers {
+                            append(HttpHeaders.Authorization, "Bearer $accessToken")
+                            append(HttpHeaders.ContentType, "application/json")
+                            append(HttpHeaders.Accept, "application/json")
+                        }
+                        body = """
+                            mutation {
+                                nyBeskjed(nyBeskjed: {
+                                    mottaker: {
+                                        altinn: {
+                                            serviceCode: "4936",
+                                            serviceEdition: "1"
+                                            virksomhetsnummer: "922658986"
+                                        } 
+                                    }
+                                    notifikasjon: {
+                                        lenke: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                                        tekst: "Notifikasjoner er nå i prod! 3 x Hipp, hurra 🎉"
+                                        merkelapp: "fager"
+                                    }
+                                    metadata: {
+                                        eksternId: "${UUID.randomUUID()}"
+                                    }
+                                }) {
+                                    ... on NyBeskjedVellykket {
+                                        id
+                                    }
+                                    ... on Error {
+                                        feilmelding
+                                    }
+                                }
+                            }
+                        """.trimIndent()
+                    }
+
+                    call.respond(response)
                 } catch (e: RuntimeException) {
                     log.error(":'(", e)
                     call.respond(HttpStatusCode.InternalServerError)
